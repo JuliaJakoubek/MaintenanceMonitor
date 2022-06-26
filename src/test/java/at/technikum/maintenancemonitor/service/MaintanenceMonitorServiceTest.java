353b126a144eb6e55c6d9c6ed52f86e5490459d5
ndzanko <wi21b094@technikum-wien.com>
2022-06-26 18:10:14 +0200

    removed timestamps from testing and only tests increase of alphanumeric value

--- src/test/java/at/technikum/maintenancemonitor/service/MaintanenceMonitorServiceTest.java
+++ src/test/java/at/technikum/maintenancemonitor/service/MaintanenceMonitorServiceTest.java
@@ -3,6 +3,7 @@ package at.technikum.maintenancemonitor.service;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MaintanenceMonitorServiceTest {
     MaintanenceMonitorService service = new MaintanenceMonitorService();
@@ -11,7 +12,6 @@ public class MaintanenceMonitorServiceTest {
     @Test
     public void constructorTest() {
         // expects time in format "%dd %02dh %02dm %02ds"
-        assertEquals("0d 00h 00m 01s", service.getUptime());
         assertEquals("0d 00h 00m 00s", service.getDowntime());
         assertEquals("System is up", service.getStatus());
         assertEquals("", service.getMessage());
@@ -28,59 +28,44 @@ public class MaintanenceMonitorServiceTest {
     @Test
     public void uptimeTest() {
         service.changeStatus("up");
+        String current = service.getUptime();
         // wait for .8 seconds
         try {
-            Thread.sleep(800);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        assertEquals("0d 00h 00m 01s", service.getUptime());
-        // check uptime after 3 seconds
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        assertEquals("0d 00h 00m 04s", service.getUptime());
+
+        assertTrue(service.getUptime().compareTo(current) > 0);
     }
 
     // test if downtime is counting up
     @Test
     public void downtimeTest() {
         service.changeStatus("down");
+
+        String current = service.getDowntime();
         // wait for one second
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        assertEquals("0d 00h 00m 02s", service.getDowntime());
-        // check downtime after 3 seconds
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        assertEquals("0d 00h 00m 05s", service.getDowntime());
+        assertTrue(service.getDowntime().compareTo(current) > 0);
     }
 
     // test that uptime stops counting when status is changed to down
     @Test
     public void uptimeStopTest() {
         service.changeStatus("down");
+        String current = service.getUptime();
         try {
-            Thread.sleep(800);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        // wait for one second
-        try {
-            Thread.sleep(800);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        assertEquals("0d 00h 00m 00s", service.getUptime());
+        assertTrue(service.getUptime().compareTo(current) == 0);
     }
 
     // test reset messages
