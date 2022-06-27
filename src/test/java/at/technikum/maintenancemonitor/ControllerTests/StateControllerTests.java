0f05b136082ecd0e9c39cf5c76ce2708d7ceaca5
ndzanko <wi21b094@technikum-wien.com>
2022-06-27 19:04:30 +0200

    removed failing unit test and changed by valid test

--- src/test/java/at/technikum/maintenancemonitor/ControllerTests/StateControllerTests.java
+++ src/test/java/at/technikum/maintenancemonitor/ControllerTests/StateControllerTests.java
@@ -15,39 +15,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class StateControllerTests {
-
-    @Test
-    void testPostMapping() throws IOException {
-        URL url = new URL("http://localhost:8080/status/set");
-        HttpURLConnection httpURLConnection;
-        try{
-            httpURLConnection = (HttpURLConnection) url.openConnection();
-            httpURLConnection.setRequestMethod("POST");
-        } catch(Exception e){
-            System.out.print("Not on localhost, therefore tests disabled");
-            return;
-        }
-
-        httpURLConnection.setDoOutput(true);
-        httpURLConnection.setRequestProperty("Content-Type", "application/json");
-        String testString = "Test";
-
-        try (OutputStream outputStream = httpURLConnection.getOutputStream()) {
-            outputStream.write(testString.getBytes());
-            outputStream.flush();
-        }
-
-        int responseCode = httpURLConnection.getResponseCode();
-
-        if (responseCode == HttpURLConnection.HTTP_OK) {
-            System.out.println("GET Response Code :: " + responseCode);
-        }
-        else {
-            System.out.println("Connection failed!");
-        }
-    }
     @Autowired
     public StateController sc;
+
+    @Test
+    void testSetState() throws IOException {
+        sc.setStatus("System is down");
+        assertEquals("System is down", sc.service.getStatus());
+
+        sc.setStatus("System is up");
+        assertEquals("System is up", sc.service.getStatus());
+    }
 
     @Test
     void testResetMessage() throws Exception{
